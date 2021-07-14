7c34f7581bb94f64c3a95dcc52aa6988dff655aa
DESKTOP-DBH06II\itlab2021 <llunipupild@bk.ru>
2021-07-14 16:15:05 +0300

    Верстка экрана настроек Контроллер диалога настроек

---
+++ client/Assets/Scripts/DronDonDon/MainMenu/UI/Settings/Service/SettingsRepository.cs
@@ -0,0 +1,13 @@
+﻿using DronDonDon.Core.Repository;
+using DronDonDon.MainMenu.UI.Settings.Model;
+
+namespace DronDonDon.MainMenu.UI.Settings.Service
+{
+    public class SettingsRepository : LocalPrefsSingleRepository<SettingsModel>
+    {
+        public SettingsRepository() : base("settingsRepository")    
+        {
+            
+        }
+    }
+}
