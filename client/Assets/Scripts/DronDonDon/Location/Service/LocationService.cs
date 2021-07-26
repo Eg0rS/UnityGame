0ae35ab62867299fe52f2cf95b805e61dbe2d8c7
Eg0rS <ehrgit@yandex.ru>
2021-07-26 10:44:29 +0300

    Подготовка билда к демо дню

--- client/Assets/Scripts/DronDonDon/Location/Service/LocationService.cs
+++ client/Assets/Scripts/DronDonDon/Location/Service/LocationService.cs
@@ -23,6 +23,7 @@ namespace DronDonDon.Location.Service
             _overlayManager.Require().ShowPreloader();
             _screenManager.LoadScreen<LocationScreen>();
             CreatedWorld();
+            
         }
         private void CreatedWorld()
         {
