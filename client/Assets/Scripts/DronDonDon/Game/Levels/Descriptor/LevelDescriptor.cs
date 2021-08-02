02261e705f181f7b231d3db5b4752276b628259b
DESKTOP-DBH06II\itlab2021 <llunipupild@bk.ru>
2021-08-02 18:56:15 +0300

    Создал префаб диалогового окна с информацией об уровне

--- client/Assets/Scripts/DronDonDon/Game/Levels/Descriptor/LevelDescriptor.cs
+++ client/Assets/Scripts/DronDonDon/Game/Levels/Descriptor/LevelDescriptor.cs
@@ -7,12 +7,24 @@ namespace DronDonDon.Game.Levels.Descriptor
         public string Id { get;  set; }
         public string Prefab { get; private set; }
         public int Order { get; private set; }
+        public int NecessaryCountChips { get; private set; }
+        public int NecessaryTime { get; private set; }
+        public int NecessaryCountStrength { get; private set; }
+        
+        public string LevelTitle { get; private set; }
+        
+        public string LevelDescription { get; private set; }
         
         public void Configure(Configuration config)
         {
             Id = config.GetString("id");
             Prefab = config.GetString("prefab");
             Order = config.GetInt("order");
+            NecessaryCountChips = config.GetInt("chips");
+            NecessaryTime = config.GetInt("time");
+            NecessaryCountStrength = config.GetInt("strength");
+            LevelTitle = config.GetString("title");
+            LevelDescription = config.GetString("description");
         }
     }
 }
---
+++ client/Assets/Scripts/DronDonDon/Resource/UI/DescriptionLevelDialog/DescriptionLevelDialog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using AgkUI.Binding.Attributes;
+using AgkUI.Dialog.Attributes;
+using AgkUI.Dialog.Service;
+using AgkUI.Element.Buttons;
+using AgkUI.Element.Text;
+using DronDonDon.Core.UI.Dialog;
+using DronDonDon.Descriptor.Service;
+using DronDonDon.Game.Levels.Descriptor;
+using IoC.Attribute;
+using IoC.Util;
+using UnityEngine;
+
+namespace DronDonDon.Resource.UI.DescriptionLevelDialog
+{
+    [UIController(PREFAB_NAME)]
+    [UIDialogFog(FogPrefabs.EMBEDED_SHADOW_FOG)]
+    public class DescriptionLevelDialog : MonoBehaviour
+    {
+        private const string PREFAB_NAME = "UI/Dialog/pfDescriptionLevelDialog@embeded";
+        
+        [Inject] 
+        private IoCProvider<DialogManager> _dialogManager;
+        
+        [UIObjectBinding("Title")]
+        private UILabel _title;
+        
+        [UIObjectBinding("Description")]
+        private UILabel _description;
+        
+        [UIObjectBinding("ChipsTask")] 
+        private UIButton _chipText;
+        
+        [UIObjectBinding("StrengthTask")] 
+        private UIButton _strengthText;
+        
+        [UIObjectBinding("TimeTask")] 
+        private UIButton _timeText;
+        
+        [UIObjectBinding("StartGameButton")] 
+        private UIButton _startGameButton;
+
+        private LevelDescriptor _levelDescriptor;
+
+        public LevelDescriptor LevelDescriptor { get; set; }
+        private void Start()
+        {
+            
+        }
+
+        private void DisplayTitle()
+        {
+            //_title.text = 
+        }
+    }
+}
