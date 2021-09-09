36653e51426e5b52cb496bd443841b87b2c47d8c
DESKTOP-STEFDPE\developers <maks_vell25@mail.ru>
2021-09-09 19:30:35 +0300

    fix(): поправил свайпы

--- client/Assets/Scripts/Drone/Location/Service/GestureService.cs
+++ client/Assets/Scripts/Drone/Location/Service/GestureService.cs
@@ -6,20 +6,9 @@ namespace Drone.Location.Service
 {
     public class GestureService : GameEventDispatcher
     {
-        private const float SwipeThreshold = 0.03f;
         private Vector2 _currentTouch;
         private Vector2 _startTouch;
-        private float _width;
-        private float _height;
-        private bool OnSwiping = false;
-
-        private void Awake()
-        {
-            _width = Screen.width;
-            _height = Screen.height;
-            _width *= _height / _width;
-            _height *= _height / _width;
-        }
+        private bool OnSwiping;
 
         private void Update()
         {
@@ -27,7 +16,7 @@ namespace Drone.Location.Service
                 return;
             }
 
-            Touch touch = Input.touches[0];
+            Touch touch = Input.touches[Input.touchCount - 1];
             switch (touch.phase) {
                 case TouchPhase.Began:
                     _startTouch = touch.position;
@@ -38,6 +27,7 @@ namespace Drone.Location.Service
                 case TouchPhase.Moved:
                     _currentTouch = touch.position;
                     DetectSwipe();
+                    OnSwiping = true;
                     break;
                 case TouchPhase.Ended:
                 case TouchPhase.Canceled:
@@ -48,53 +38,24 @@ namespace Drone.Location.Service
                 case TouchPhase.Stationary:
                     break;
             }
-            /*
-if UNITY_EDITOR
-            else
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    _startTouch = Input.mousePosition;
-                    _currentTouch = Input.mousePosition;
-                }
-                else if (Input.GetMouseButton(0))
-                {
-                    _currentTouch = Input.mousePosition;
-                    DetectSwipe();
-                }
-                else if (Input.GetMouseButtonUp(0))
-                {
-                    _startTouch = Vector2.zero;
-                    _currentTouch = Vector2.zero;
-                }
-            }
-endif
-*/
         }
 
         private void DetectSwipe()
         {
             Vector2 swipeVector = _currentTouch - _startTouch;
-            float lengthX = Mathf.Abs(swipeVector.x / _width);
-            float lengthY = Mathf.Abs(swipeVector.y / _height);
-
-            if (lengthX <= SwipeThreshold && lengthY <= SwipeThreshold) {
-                return;
-            }
-            OnSwiping = true;
             swipeVector = RoundVector(swipeVector);
-            //_startTouch = _currentTouch;
             Dispatch(new WorldEvent(WorldEvent.SWIPE, swipeVector));
-            OnSwiping = true;
         }
 
         private Vector2 RoundVector(Vector2 vector)
         {
             vector = vector.normalized;
+            
             vector.x = Mathf.Round(vector.x);
             vector.y = Mathf.Round(vector.y);
 
             return vector;
         }
+
     }
 }
