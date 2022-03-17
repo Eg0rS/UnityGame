4599340cc22b998e18f8befd44abc4bf2c383df9
e.salnikov <ehrgit@yandex.ru>
2022-03-17 15:14:26 +0300

    future(): ui

--- client/Assets/Scripts/Drone/LevelMap/LevelDialogs/FailLevelDialog.cs
+++ client/Assets/Scripts/Drone/LevelMap/LevelDialogs/FailLevelDialog.cs
@@ -4,12 +4,14 @@ using AgkUI.Dialog.Attributes;
 using AgkUI.Dialog.Service;
 using AgkUI.Element.Buttons;
 using AgkUI.Element.Text;
+using AgkUI.Screens.Service;
+using DG.Tweening;
 using Drone.Core.UI.Dialog;
-using Drone.Location.Service.Game;
 using Drone.Location.Service.Game.Event;
 using Drone.Location.World;
-using GameKit.World;
+using Drone.MainMenu.UI.Screen;
 using IoC.Attribute;
+using UnityEngine;
 using Image = UnityEngine.UI.Image;
 
 namespace Drone.LevelMap.LevelDialogs
@@ -19,9 +21,12 @@ namespace Drone.LevelMap.LevelDialogs
     public class FailLevelDialog : GameEventDispatcher
     {
         private const string PREFAB_NAME = "UI_Prototype/Dialog/Respawn/pfRespawnDialog@embeded";
-
+        private const float TIME_FOR_END = 10f;
         private string _levelId;
 
+        private float _timeForEndLeft = TIME_FOR_END;
+        [Inject]
+        private ScreenManager _screenManager;
         [Inject]
         private DialogManager _dialogManager;
 
@@ -41,6 +46,28 @@ namespace Drone.LevelMap.LevelDialogs
         public void Init()
         {
             _restartButton.onClick.AddListener(RespawnButtonClick);
+        }
+
+        private void Update()
+        {
+            _timeForEndLeft -= Time.unscaledDeltaTime;
+            if (_timeForEndLeft <= 0f) {
+                OnComplete();
+                return;
+            }
+            float percent = _timeForEndLeft / TIME_FOR_END;
+            if (percent <= 0.3f) {
+                _filedArea.color = new Color(1, 0.02745098f, 0.02745098f);
+                _timerLabel.color = new Color(1, 0.02745098f, 0.02745098f);
+            }
+            _filedArea.fillAmount = _timeForEndLeft / TIME_FOR_END;
+            _timerLabel.text = _timeForEndLeft.ToString("F1");
+        }
+
+        private void OnComplete()
+        {
+            _dialogManager.Hide(this);
+            _screenManager.LoadScreen<MainMenuScreen>();
         }
 
         private void RespawnButtonClick()
